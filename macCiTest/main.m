d6193ba161a46f170ed652fad0c16ed455a78d32
gnuhub <gnuhub@gmail.com>
2014-07-17 20:49:30 +0800

    Initial Commit

---
+++ macCiTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  macCiTest
+//
+//  Created by gnuhub on 14-7-17.
+//  Copyright (c) 2014年 com.gnuhub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
